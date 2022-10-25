5fa229401d0d2a3ba1efc41254ce4a75e8b21626
Hemanggupta <96711288+Hemanggupta@users.noreply.github.com>
2022-10-25 16:33:11 +0530

    DOM

---
+++ Part2-DOM/file1-selectors.js
@@ -0,0 +1,15 @@
+// console.log("Hello");
+// console.dir(document);
+// console.log(document);
+
+const mainHeading = document.getElementById('main-heading');
+console.log("getElementById", mainHeading);
+
+const mainHeading2 = document.querySelector('#main-heading');
+console.log("querySelector use  #/. ", mainHeading2);
+
+const navItem = document.querySelector('.nav-item');
+console.log("Multiple elements with same class  ", navItem)
+
+const navItem2 = document.querySelectorAll('.nav-item');
+console.log("Multiple elements with same class  ", navItem2)
