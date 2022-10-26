bec955e7d16ab46fa2be9466fc50606b6104516a
Hemang <hemang2608@gmail.com>
2022-10-26 17:43:54 +0530

    Update file1-selectors.js

--- Part2-DOM/file1-selectors.js
+++ Part2-DOM/file1-selectors.js
@@ -7,7 +7,6 @@ console.log("getElementById", mainHeading);
 
 const mainHeading2 = document.querySelector('#main-heading');
 console.log("querySelector use  #/. ", mainHeading2);
-
 const navItem = document.querySelector('.nav-item');
 console.log("Multiple elements with same class  ", navItem)
 
