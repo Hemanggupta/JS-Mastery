c1eeee4c5f510bc25be030241361b2723c65efd4
Hemang <hemang2608@gmail.com>
2022-10-29 21:55:35 +0530

    Events

---
+++ Part2-DOM/file7-events.js
@@ -0,0 +1,17 @@
+const btn = document.querySelector('.btn-headline');
+
+// btn.onclick = function () {
+//   console.log("clicked")
+// }
+
+console.log(btn);
+
+// btn.addEventListener('click', clicked);
+
+// function clicked() {
+//   console.log('clicked');
+// }
+
+
+// arrow function
+btn.addEventListener('click', () => console.log('clicked'));
